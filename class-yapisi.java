07d09208a5c812f6fd54002f21388313d4b4c28b
samet-unsal <samettunsal@gmail.com>
2024-09-20 11:28:22 +0300

    Update class-yapisi.java

--- class-yapisi.java
+++ class-yapisi.java
@@ -7,9 +7,12 @@ public class Kutu {
 	
 	public Kutu(float genislik, float yukseklik, float derinlik) {//parantez içindekiler lokal değişkendir, this keywordü ile bu değişkeni
 																//class içindeki global değişken olduğunu işaret ediyoruz.
-		this.genislik=genislik;
-		this.yukseklik=yukseklik;
-		this.derinlik=derinlik;
+		/*this.genislik=genislik; //bu şekilde tanım yaparsak ilk atamada negatif değerler atayabiliriz.
+		this.yukseklik=yukseklik;//parametreleri direkt set metotuyla atarsak kontrolü direkt yapacak.
+		this.derinlik=derinlik;*/
+		setGenislik(genislik);
+		setYukseklik(yukseklik);
+		setDerinklik(derinlik);
 	}
 
 	public float hacimHesapla() {
@@ -27,5 +30,31 @@ public class Kutu {
 	
 	public float getGenislik() {
 		return this.genislik;
+	}
+	
+	public void setYukseklik(float yukseklik){
+		if (yukseklik > 0) {
+			this.yukseklik = yukseklik;
+		}
+		else {
+			this.yukseklik = 1;
+		}
+	}
+	
+	public float getYukselik() {
+		return this.yukseklik;
+	}
+	
+	public void setDerinklik(float derinlik){
+		if (derinlik > 0) {
+			this.derinlik = derinlik;
+		}
+		else {
+			this.derinlik = 1;
+		}
+	}
+	
+	public float getDerinlik() {
+		return this.derinlik;
 	}
 }
