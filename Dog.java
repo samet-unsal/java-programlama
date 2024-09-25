66b9054301a54b8b3d68121f00d6894c4e30914e
samet-unsal <samettunsal@gmail.com>
2024-09-25 12:08:27 +0300

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,14 @@
+
+public class Dog extends Animal {
+	
+	public Dog() {
+		noise = "Hav hav";
+	}
+	
+	public void doYourThing() {
+		identifySelf();
+		System.out.println("Ben bir köpeğim.");
+		System.out.println(noise);
+	}
+
+}
