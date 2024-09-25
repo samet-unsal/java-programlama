090c68149a9fedddb43f00691230fa202c9e7e4c
samet-unsal <samettunsal@gmail.com>
2024-09-25 12:08:16 +0300

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,14 @@
+
+public class Cat extends Animal {
+	
+	public Cat() {
+		noise = "Miyav";
+	}
+	
+	public void doYourThing() {
+		identifySelf();
+		System.out.println("Ben bir kediyim.");
+		System.out.println(noise);
+	}
+
+}
