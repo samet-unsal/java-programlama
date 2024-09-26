69ade4cd1284e0248b1d9baf7ac25942a548774b
samet-unsal <samettunsal@gmail.com>
2024-09-26 12:04:22 +0300

    Update TestSinifi.java

--- TestSinifi.java
+++ TestSinifi.java
@@ -18,6 +18,22 @@ public class TestSinifi {
 		System.out.println("Alan: " + c1.area());
 		System.out.println("----------------");
 		
+		System.out.println("----------------");
+		System.out.println("ANIMAL FARM");
+		
+		Animal1[] hayvanlar = new Animal1[3];
+		hayvanlar[0] = new Cat1();
+		hayvanlar[1] = new Dog1();
+		hayvanlar [2] = new Kus();
+		
+		for (int i = 0; i< hayvanlar.length; i++) {
+			hayvanlar[i].makeNoise();
+			if (hayvanlar[i] instanceof Dog1) {//bark() fonksiyonu sadece Dog1 sınıfına ait olduğu için nesneyi Dog1 sınıfına cast etmemiz gerekir.
+				Dog1 d = (Dog1) hayvanlar[i];
+				d.bark();
+			}
+		}
+				
 	}
 
 }
